7a20068743b1e229e86b0bdd0534ba7777646b7d
ramiro <ceinnova13458@innovaccion.mx>
2023-05-30 22:26:16 -0600

    ConfirmarCuenta.jsx, OlvidePassword.jsx y Registrar.jsx creados. Rutas para los componentes anteriores creadas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthLayout from './layout/AuthLayout';
 import Login from './layout/paginas/Login';
+import Registrar from './layout/paginas/Registrar';
+import OlvidePassword from './layout/paginas/OlvidePassword';
+import ConfirmarCuenta from './layout/paginas/ConfirmarCuenta';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path='/' element={<AuthLayout />}>
           <Route index element={<Login />} />
+          <Route path='registrar' element={<Registrar />} />
+          <Route path='olvide-password' element={<OlvidePassword />} />
+          <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
         </Route>
       </Routes>
     </BrowserRouter>
