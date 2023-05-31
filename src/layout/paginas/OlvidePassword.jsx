6b6eff61c2f15d7792b1d5689ee5a1debe3f2124
ramiro <ceinnova13458@innovaccion.mx>
2023-05-31 14:03:18 -0600

    Validación de email agregada en OlvidePassword.jsx en la función handleSubmit(). Consumo de API para enviar un email con instrucciones hecho en habdleSubmit() en el mismo archivo .jsx

--- src/layout/paginas/OlvidePassword.jsx
+++ src/layout/paginas/OlvidePassword.jsx
@@ -1,6 +1,33 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Alerta from '../../components/Alerta';
+import clienteAxios from '../../config/axios';
 
 const OlvidePassword = () => {
+  const [email, setEmail] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email === '' || email.length < 6) {
+      setAlerta({ msg: 'El email es obligatorio', error: true });
+      return;
+    }
+    try {
+      const { data } = await clienteAxios.post(
+        '/veterinarios/olvide-password',
+        {
+          email,
+        }
+      );
+      setAlerta({ msg: data.msg });
+    } catch (error) {
+      setAlerta({ msg: error.response.data.msg, error: true });
+    }
+  };
+
+  const { msg } = alerta;
   return (
     <>
       <div>
@@ -10,7 +37,8 @@ const OlvidePassword = () => {
         </h1>
       </div>
       <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-        <form>
+        {msg && <Alerta alerta={alerta} />}
+        <form onSubmit={handleSubmit}>
           <div className='my-5'>
             <label className='uppercase text-gray-600 block text-xl font-bold'>
               Email
@@ -19,6 +47,8 @@ const OlvidePassword = () => {
               type='email'
               placeholder='Email de Registro'
               className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <input
