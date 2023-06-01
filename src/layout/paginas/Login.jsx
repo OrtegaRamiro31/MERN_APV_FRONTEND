fcc54771d6b7439eca4cc2bcace7592c26117e51
ramiro <ceinnova13458@innovaccion.mx>
2023-05-31 20:20:09 -0600

    Validación y logueo de usuario añadida y probada en Login.jsx

--- src/layout/paginas/Login.jsx
+++ src/layout/paginas/Login.jsx
@@ -1,7 +1,39 @@
 import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import { useState } from 'react';
+import Alerta from '../../components/Alerta';
+import clienteAxios from '../../config/axios';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if ([email, password].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true,
+      });
+
+      return;
+    }
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/login', {
+        email,
+        password,
+      });
+      localStorage.setItem('token', data.token);
+    } catch (error) {
+      setAlerta({ msg: error.response.data.msg, error: true });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -11,7 +43,8 @@ const Login = () => {
         </h1>
       </div>
       <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-        <form>
+        {msg && <Alerta alerta={alerta} />}
+        <form onSubmit={handleSubmit}>
           <div className='my-5'>
             <label className='uppercase text-gray-600 block text-xl font-bold'>
               Email
@@ -20,6 +53,8 @@ const Login = () => {
               type='email'
               placeholder='Email de Registro'
               className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className='my-5'>
@@ -30,6 +65,8 @@ const Login = () => {
               type='password'
               placeholder='Tu Password'
               className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <input
