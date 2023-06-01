102c77df104307236a708e21363db4dcdb9b8816
ramiro <ceinnova13458@innovaccion.mx>
2023-05-31 19:47:17 -0600

    Context crreado en AuthProvicer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Registrar from './layout/paginas/Registrar';
 import OlvidePassword from './layout/paginas/OlvidePassword';
 import ConfirmarCuenta from './layout/paginas/ConfirmarCuenta';
 import NuevoPassword from './layout/paginas/NuevoPassword';
+import { AuthProvider } from './context/AuthProvider';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path='registrar' element={<Registrar />} />
-          <Route path='olvide-password' element={<OlvidePassword />} />
-          <Route path='olvide-password/:token' element={<NuevoPassword />} />
-          <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path='registrar' element={<Registrar />} />
+            <Route path='olvide-password' element={<OlvidePassword />} />
+            <Route path='olvide-password/:token' element={<NuevoPassword />} />
+            <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
