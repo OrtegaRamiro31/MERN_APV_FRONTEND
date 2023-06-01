e0a2194d0a3fae6b66592314856ac2273ef44c80
ramiro <ceinnova13458@innovaccion.mx>
2023-06-01 10:21:26 -0600

    Elementos agregados a ListadoPacientes.jsx

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,5 +1,33 @@
+import usePacientes from '../hooks/usePacientes';
+
 const ListadoPacientes = () => {
-  return <div>ListadoPacientes</div>;
+  const { pacientes } = usePacientes();
+  console.log(pacientes);
+  return (
+    <>
+      {pacientes.length ? (
+        <>
+          <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Administra tus {''}
+            <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+          </p>
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center'>No hay Pacientes</h2>
+
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Comienza agregando pacientes {''}
+            <span className='text-indigo-600 font-bold'>
+              Y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ListadoPacientes;
