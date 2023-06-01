5bc112127862b81eae91a7dd707e445657af3b4e
ramiro <ceinnova13458@innovaccion.mx>
2023-06-01 09:29:00 -0600

    Bug que no redireccionaba a la página de admin al loguearse solucionado

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -9,6 +9,8 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [alerta, setAlerta] = useState({});
 
+  const { setAuth } = useAuth();
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -29,7 +31,7 @@ const Login = () => {
         password,
       });
       localStorage.setItem('token', data.token);
-
+      setAuth(data);
       navigate('/admin');
     } catch (error) {
       setAlerta({ msg: error.response.data.msg, error: true });
