90502614090f8d262e867abc13d11ad2ca46781b
ramiro <ceinnova13458@innovaccion.mx>
2023-05-31 19:54:57 -0600

    AuthProvider en AuthProvider.jsx ahora retorna valores. useAuth() en useAuth.jsx ahora retorna un Context.

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -4,7 +4,16 @@ const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthProvider };
